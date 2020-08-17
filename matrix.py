b52c7436f1206d8df6e62fb7e085b8289a787380
DTomusk <david.tomusk@gmail.com>
2020-08-17 15:07:56 +0100

    Operation overloading and zeros and identity generators

--- matrix.py
+++ matrix.py
@@ -3,14 +3,42 @@ import math
 class Matrix():
 	def __init__(self, data):
 		self.data = data
+		self.col = len(data)
+		self.row = len(data[0])
 
 	def __str__(self):
 		string = ""
-		for x in self.data:
-			for y in x:
-				string += str(round(y,4)) + " "
+		for i in range(self.row):
+			for j in range(self.col):
+				string += str(round(self.data[j][i])) + " "
 			string += "\n"
 		return string 
+
+	def __mul__(self, other):
+		if self.col != other.row:
+			return 0
+		result = Matrix.zeros([self.row, other.col])
+		for i in range(self.row):
+				for j in range(other.col):
+					for k in range(self.col):
+						result.data[j][i] += self.data[k][i]*other.data[j][k]
+		return result
+
+	@staticmethod
+	def zeros(dim):
+		matrix = []
+		for j in range(dim[1]):
+			matrix.append([])
+			for i in range(dim[0]):
+				matrix[j].append(0)
+		return Matrix(matrix)
+
+	@staticmethod
+	def identity(n):
+		matrix = Matrix.zeros([n,n])
+		for i in range(n):
+			matrix.data[i][i] = 1
+		return matrix 
 
 	@staticmethod
 	def conversion(n):
--- start.py
+++ start.py
@@ -6,5 +6,14 @@ def main():
 	conversion = Matrix.conversion(n)
 	print(conversion)
 
+	A = Matrix([[5,1],[3,7]])
+	B = Matrix([[6,6],[4,3]])
+	print(A)
+	print(B)
+	print(A*B)
+
+	I = Matrix.identity(5)
+	print(I)
+
 if __name__=="__main__":
 	main()
